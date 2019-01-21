cf840ca019eefc09ae20d806b66324d52191137f
KokosnussDEV <kcvw@icloud.com>
2019-01-21 22:26:30 +0100

    Added values

--- config/config.js
+++ config/config.js
@@ -1,7 +1,9 @@
 let config = {
-    "clientToken": "",
-    "clientID": "",
-    "version": ""
+    "clientToken": "NTM2OTc4NzQ5MDIzOTc3NDgy.DyekPw.2DBxazjZKFwJDFm9BdFjtTE5STA",
+    "clientID": "536978749023977482",
+    "version": "v0.0.1",
+    "logChannel": "536989961514254357"
 }
 console.log(config)
+
 module.exports = config

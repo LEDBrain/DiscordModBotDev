0081b64346a047f07de0cd711b1e855c47fd8bd9
KokosnussDEV <kcvw@icloud.com>
2019-01-23 21:33:39 +0100

    Changed v.

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 let config = {
     "clientToken": "NTM2OTc4NzQ5MDIzOTc3NDgy.DyekPw.2DBxazjZKFwJDFm9BdFjtTE5STA",
     "clientID": "536978749023977482",
-    "version": "v0.0.1",
+    "version": "v0.1.0",
     "logChannel": "532615243776655370" // 536989961514254357 <== Server Version
-}
-console.log(config)
+};
+console.log(config);
 
-module.exports = config
+module.exports = config;
---
+++ config/versions.js
@@ -0,0 +1,4 @@
+let versions = {
+    "v.0.0.1": "Version ohne alles",
+    "v.0.1.0": "Aktuelleste Version"
+};

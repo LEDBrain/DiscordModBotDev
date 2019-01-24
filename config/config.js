6727a66b1968757907c9e96c21f358b37572f312
KokosnussDEV <kcvw@icloud.com>
2019-01-24 21:29:19 +0100

    Added prefix

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ let config = {
     "clientToken": "NTM2OTc4NzQ5MDIzOTc3NDgy.DyekPw.2DBxazjZKFwJDFm9BdFjtTE5STA",
     "clientID": "536978749023977482",
     "version": "v0.1.0",
+    "prefix": "!",
     "logChannel": "536989961514254357" // 532615243776655370 <= Test Server
 };
 console.log(config);

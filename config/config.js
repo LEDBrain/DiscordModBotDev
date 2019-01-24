8c1f8ece6e5b7eb615c08d223bbf890982f17cc6
KokosnussDEV <kcvw@icloud.com>
2019-01-24 17:56:46 +0100

    Changed to Server

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ let config = {
     "clientToken": "NTM2OTc4NzQ5MDIzOTc3NDgy.DyekPw.2DBxazjZKFwJDFm9BdFjtTE5STA",
     "clientID": "536978749023977482",
     "version": "v0.1.0",
-    "logChannel": "532615243776655370" // 536989961514254357 <== Server Version
+    "logChannel": "536989961514254357" // 532615243776655370 <= Test Server
 };
 console.log(config);
 

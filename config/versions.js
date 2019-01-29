f7e869c15ca89027c80ba86a730ca62bb43d8d07
Konstantin <kcvw@icloud.com>
2019-01-29 19:46:41 +0100

    Versionen geupdated

--- config/versions.js
+++ config/versions.js
@@ -1,5 +1,7 @@
 let versions = {
     "v.0.0.1": "Version ohne alles",
     "v.0.1.0": "Nur LOG",
-    "v.1.0.0": "Mutesystem Update"
+    "v.1.0.0": "Mutesystem Update",
+    "v.1.0.1": "Issue #2 wurde reingepached",
+    "v.1.1.0": "Kick System hinzugefügt"
 };

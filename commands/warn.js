6d87d119f511b3508f48148f47117073a65c7812
Konstantin <kcvw@icloud.com>
2019-04-06 20:59:23 +0200

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -17,31 +17,34 @@ module.exports = {
 
         if (!member) return params.message.channel.send(`Bitte gebe ein User an! Format: \`${params.prefix}warn <@user> <Grund>\``);
 
-        if (member.roles.has(config.staffrole)) return params.message.channel.send("Du kannst keine Administratoren oder Moderatoren Warnen!");
+        if (member.roles.has(params.staffrole)) return params.message.channel.send("Du kannst keine Administratoren oder Moderatoren Warnen!");
 
         if (!reason) return params.message.channel.send(`Bitte gebe einen Grund an! Format: \`${params.prefix}warn <@user> <Grund>\``);
 
-        db.query("SELECT `warns` FROM `warnungen` WHERE `id` = ?", member.id, (err, result) => {
+        db.query("SELECT `warns` FROM `warnungen` WHERE `id` = ?", [member.id], (err, result) => {
             if (err) throw (err);
             let warns = result[0] ? result[0].warns + 1 : 1;
-            let sql = result[0] ? mysql.format("INSERT INTO `warnungen` (`id`, `username`, `warns`) VALUE (?, ?, 1)", [member.id, member.user.username]): mysql.format("UPDATE `warnungen` SET `warns` = ? WHERE `id` = ?", [warns, member.id]);
+            let sql = result[0] ? mysql.format("INSERT INTO `warnungen` (`id`, `username`, `warns`) VALUE (?, ?, ?)", [member.id, member.user.username, warns]): mysql.format("UPDATE `warnungen` SET `warns` = ? WHERE `id` = ?", [warns, member.id]);
             db.query(sql, error => {
                 if (error) throw (error);
+
                 params.message.channel.send(`${member.user} wurde ${warns === 1 ? "zum ersten mal " : ""}verwarnt. Jetzige Warns: ${warns}`);
+
                 let warnEmbed = new Discord.RichEmbed()
                         .setTitle(`Ein User wurde ${warns === 1 ? "zum ersten mal " : ""}verwarnt!`)
                         .setColor(0xf4eb42)
                         .addField("User", `${member.user}/${member.id}`)
                         .addField("Moderator", `${params.message.author}/${params.message.author.id}`)
                         .addField("Warns", warns)
-                        .addField("Grund", `\`\`\`reason\`\`\``)
+                        .addField("Grund", `\`\`\`${reason}\`\`\``)
                         .setFooter(`${params.appName} ${params.version}`)
                         .setTimestamp();
 
                 params.logChannel.send({ embed: warnEmbed });
-                db.end();
-                console.log("Disconnected");
+    
             });
+            db.end();
+            console.log("Disconnected");
         });
     }
 };

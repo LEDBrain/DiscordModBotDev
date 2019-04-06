a0d7eeb2a6b373a21550c71741ddefe67cbe132e
Jan <jan.boehmerle@ledbrain.de>
2019-04-06 15:28:24 +0200

    ES6 improvement

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,11 +1,12 @@
 module.exports = {
-    do: async function(params) {
+    do: function(params) {
         if (!params.message.member.roles.has(params.ownerID)) return params.message.channel.send("Nice try! :joy:");
 
         try {
-            await params.message.channel.send("Der Bot fährt herunter...");
-
-            params.client.destroy();
+            params.message.channel.send("Der Bot fährt herunter...")
+                .then(() => {
+                    params.client.destroy();
+                });
         } catch (e) {
             params.message.channel.send(`ERROR: ${e.message}`);
         }
